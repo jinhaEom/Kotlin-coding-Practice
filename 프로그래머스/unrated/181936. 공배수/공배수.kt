032c5c7604e7451070d3b27142ca3be0fb89c985
Eom Jinha <84216838+jinhaEom@users.noreply.github.com>
2023-11-27 17:28:18 +0900

    [level unrated] Title: 공배수, Time: 0.01 ms, Memory: 60.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181936. 공배수/공배수.kt
@@ -0,0 +1,12 @@
+class Solution {
+    fun solution(number: Int, n: Int, m: Int): Int {
+        var answer: Int = 0
+        if(number%n == 0 && number%m == 0){
+            return 1
+        }else{
+            return 0
+
+        }
+        
+    }
+}
