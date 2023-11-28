7462628c7744e89ffed312047c1de3a15322f37f
Eom Jinha <84216838+jinhaEom@users.noreply.github.com>
2023-11-28 10:11:35 +0900

    [level unrated] Title: flag에 따라 다른 값 반환하기, Time: 0.01 ms, Memory: 61 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181933. flag에 따라 다른 값 반환하기/flag에 따라 다른 값 반환하기.kt
@@ -0,0 +1,12 @@
+class Solution {
+    fun solution(a: Int, b: Int, flag: Boolean): Int {
+        if(flag == true){
+            var result = a+b
+            return result
+        }else{
+            var result = a-b
+            return result
+        }
+   
+    }
+}
