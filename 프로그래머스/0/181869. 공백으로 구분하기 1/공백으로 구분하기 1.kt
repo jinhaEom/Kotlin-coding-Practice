ee5b80892c6fa374af0b21ad2f5c2f651954cadb
Eom Jinha <84216838+jinhaEom@users.noreply.github.com>
2024-07-05 15:24:15 +0900

    [level 0] Title: 공백으로 구분하기 1, Time: 14.81 ms, Memory: 61.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181869. 공백으로 구분하기 1/공백으로 구분하기 1.kt
@@ -0,0 +1,8 @@
+class Solution {
+    fun solution(my_string: String): Array<String> {
+        var answer: Array<String> = arrayOf<String>()
+        
+            return my_string.split(" ").toTypedArray()
+
+    }
+}
