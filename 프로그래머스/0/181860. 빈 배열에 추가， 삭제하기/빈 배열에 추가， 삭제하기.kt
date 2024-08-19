8fda8b973c03f577118289aa46ee7e6cab4706c1
Eom Jinha <84216838+jinhaEom@users.noreply.github.com>
2024-08-19 11:59:32 +0900

    [level 0] Title: 빈 배열에 추가, 삭제하기, Time: 9.15 ms, Memory: 61.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181860. 빈 배열에 추가， 삭제하기/빈 배열에 추가， 삭제하기.kt
@@ -0,0 +1,20 @@
+class Solution {
+    fun solution(arr: IntArray, flag: BooleanArray): IntArray {
+        val X = mutableListOf<Int>()
+        
+        for(i in arr.indices){
+            if(flag[i]){
+                repeat(arr[i]*2){
+                    X.add(arr[i])
+                }
+            }else{
+                repeat(arr[i]){
+                    if(X.isNotEmpty()){
+                        X.removeAt(X.size -1)
+                    }
+                }
+            }
+        }
+        return X.toIntArray()
+    }
+}
