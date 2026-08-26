b921b7eaa15e21ca4fa38621789e809c32feedb6
Eom Jinha <84216838+jinhaEom@users.noreply.github.com>
2026-08-26 17:00:15 +0900

    [level 2] Title: 전화번호 목록, Time: 100.29 ms, Memory: 97.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -0,0 +1,11 @@
+function solution(phone_book) {
+    let answer = true
+    phone_book.sort()
+    
+    for(let i =0; i<phone_book.length -1; i++){
+        if(phone_book[i+1].startsWith(phone_book[i])){
+            answer = false
+        }
+    }
+    return answer
+}
