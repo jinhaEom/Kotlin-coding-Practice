b9de3ed3a158a673b0840e1aa3edb328cb9ddc45
Eom Jinha <84216838+jinhaEom@users.noreply.github.com>
2026-08-26 13:59:04 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 91.26 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,21 +1,10 @@
 function solution(participant, completion) {
-    var answer = '';
-    const hashMap = new Map();
+    participant.sort()
+    completion.sort()
     
-    for(const name of participant){
-        if(hashMap.get(name)){
-            hashMap.set(name,hashMap.get(name)+1);
-        }else{
-            hashMap.set(name,1);
+    for(const i in participant){
+        if(completion[i] !=participant[i]){
+            return participant[i]
         }
     }
-    for(const name of completion){
-        hashMap.set(name,hashMap.get(name)-1)
-    }
-    for(const [name,count] of hashMap){
-        if(count === 1){
-            return name
-        }
-    }
-    return answer;
 }
